b231ba7e44da6a5ffdcc81251de1a287664fb539
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2021-12-23 11:52:03 -0800

    0x02-100

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    num = len(sys.argv)
+    if num != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit (1)
+    elif num == 4:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if sys.argv[2] == '+':
+            print("{} {} {} = {}".format(a, sys.argv[2], b, add(a, b)))
+        elif sys.argv[2] == '-':
+             print("{} {} {} = {}".format(a, sys.argv[2], b, sub(a, b)))
+        elif sys.argv[2] == '*':
+             print("{} {} {} = {}".format(a, sys.argv[2], b, mul(a, b)))
+        elif sys.argv[2] == '/':
+             print("{} {} {} = {}".format(a, sys.argv[2], b, div(a, b)))
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            exit (1)
