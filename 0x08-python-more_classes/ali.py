4a3d849dfc3f2495cf08657d5951c878b081a57b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-01-24 14:23:09 -0800

    0x08-02

--- 0x08-python-more_classes/ali.py
+++ 0x08-python-more_classes/ali.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-""" Classe Rectangle"""
+"""Module 1-rectangle """
 
 
 class Rectangle:
-    """ class Rectangle """
+    """Rectangle class"""
     def __init__(self, width=0, height=0):
         self.width = width
         self.height = height
@@ -14,7 +14,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -26,16 +26,17 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+
+        if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
     def area(self):
-        return self.__height * self.__width
+        return self.__width * self.__height
 
     def perimeter(self):
-        if self.__height == 0 or self.__width ==0:
+        if self.__height == 0 or self.__width == 0:
             return 0
-        return (self.__height * 2) + (self.__width * 2)
+        return 2 * (self.__width + self.__height)
