ccd1093afd886a1a87ed3afeafe039e785371c75
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-01-26 07:59:52 -0800

    0x07-00

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Module add-integer"""
+
+
 def add_integer(a, b=98):
+
     if type(a) not in [int, float]:
         raise TypeError('a must be an integer')
     if type(b) not in [int, float]:
