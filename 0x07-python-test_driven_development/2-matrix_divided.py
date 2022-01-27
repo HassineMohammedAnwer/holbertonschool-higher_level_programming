b7ab6bfdb9d391fd9bca26c0c7faf7f135533172
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-01-27 06:57:30 -0800

    0x07-02

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,19 +1,26 @@
 #!/usr/bin/python3
-""" Module Matrix Devided """
+"""
+Module matrix_divided
+Divides each element of a matrix of numbers by a number
+"""
 
 
 def matrix_divided(matrix, div):
-    """ matrix_devided function """
+    """Returns a new matrix (list of list)
+    with the result of the division of matrix by div
+    rounded to 2 decimal places.
+    """
+
     if not isinstance(matrix, list) or len(matrix) == 0 or not matrix[0]:
         raise TypeError("matrix must be a matrix (list of lists) " +
                         "of integers/floats")
 
     for row in matrix:
         if len(row) == 0:
-            raise TypeError("matrix must be a matrix " +
-                            "(lists of lists) of integers/floats")
+            raise TypeError("matrix must be a matrix (list of lists) " +
+                            "of integers/floats")
         for x in row:
-            if type(x) not in [int, float] or not x:
+            if type(x) is not int and type(x) is not float:
                 raise TypeError("matrix must be a matrix (list of lists) " +
                                 "of integers/floats")
 
@@ -23,12 +30,12 @@ def matrix_divided(matrix, div):
     if not all(elem == len_rows[0] for elem in len_rows):
         raise TypeError("Each row of the matrix must have the same size")
 
-    if type(div) not in [int, float]:
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div == 0:
         raise ZeroDivisionError("division by zero")
 
-
     new_matrix = [[round(x / div, 2) for x in row] for row in matrix]
+
     return new_matrix
