1cafb6b3a0af38796c1cae072ef0669b3cae4d6b
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-01 06:56:03 -0800

    0x06-06

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -42,11 +42,11 @@ class Square:
     def my_print(self):
         if self.__size == 0:
             print()
-        for i in range(0, self.position[1])
+        for i in range(0, self.__position[1]):
             print()
-        for i in range(0, self.size):
-            for j in range(0, self.position[0])
+        for i in range(0, self.__size):
+            for j in range(0, self.__position[0]):
                 print("_", end="")
-            for j in range(0, self.size):
+            for j in range(0, self.__size):
                 print("#", end="")
             print()
