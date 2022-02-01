a231c23f3ceefe9375c2bd5b2d8fc07add22460a
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-01 10:05:35 -0800

    0x0B-00

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+"""READ a file"""
+
+
 def read_file(filename=""):
+    """ function name read_file"""
     with open(filename, "r", encoding="utf-8") as f:
         print(f.read(), end="")
