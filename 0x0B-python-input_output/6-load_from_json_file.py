b4128e0f10db790809bc3dd89b558b0b12c52445
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-01 11:02:28 -0800

    0x0B-06

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """ function name load_from_json_file"""
-    with open(filename, "r", encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         json.load(f)
