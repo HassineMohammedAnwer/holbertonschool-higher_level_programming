0caf6bb931d1d8f5eaeefe789c6a6eb385e8958c
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-01 10:15:26 -0800

    0x0B-01

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""write a file"""
+
+
+def write_file(filename="", text=""):
+    """ function name write_file"""
+    with open(filename, "w", encoding="utf-8") as f:
+       return(f.write(text))
