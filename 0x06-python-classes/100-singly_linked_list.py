1eb90dace399c05daa35561a5a73a5f9d79fadee
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-01 08:17:40 -0800

    0x06-100

---
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python3
+"""class Node."""
+
+
+class Node:
+    """name of class node"""
+
+    def __init__(self, data, next_node=None):
+        self.__data = data
+        self.__next_node = next_node
+
+    @property
+    def data(self):
+        return self.__data
+
+    @data.setter
+    def data(self, value):
+        if type(value) != int:
+            raise TypeError("data must be an integer")
+        else:
+            self.__data = value
+
+    @property
+    def next_node(self):
+        return self.__next_node
+
+    @next_node.setter
+    def next_node(self, value):
+        if value is not None and type(value) != Node:
+            raise TypeError("next_node must be a Node object")
+        else:
+            self.__next_node = value
+
+"""class SinglyLinkedList."""
+
+
+class SinglyLinkedList:
+    """name of class singlylinkedlist"""
+
+    def __init__(self):
+        self.head = None
+
+    def __str__(self):
+        my_str = ""
+        node = self.head
+        while node != None:
+            my_str += str(node.data)
+            my_str += '\n'
+            node = node.next_node
+        return my_str[:-1]
+
+    def sorted_insert(self, value):
+        new_node = Node(value)
+        if self.head is None:
+            self.head = new_node
+            return
+        if value < self.head.data:
+            new_node.next_node = self.head
+            self.head = new_node
+            return
+        node = self.head
+        while node.next_node and node.next_node.data < value:
+            node = node.next_node
+        new_node.next_node = node.next_node
+        node.next_node = new_node
+
