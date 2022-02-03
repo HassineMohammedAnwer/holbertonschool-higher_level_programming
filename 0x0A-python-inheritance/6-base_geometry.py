efcbbb827bcd0eb5adb50c5929d7a3ac4862c62a
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-02-03 09:38:47 -0800

    0x0A-06

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" class BaseGeometry """
+
+
+class BaseGeometry():
+    """  contains instance method area """
+
+    def area(self):
+        raise Exception('area() is not implemented')
