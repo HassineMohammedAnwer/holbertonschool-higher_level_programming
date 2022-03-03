e3a5e35617c7a7e985c6d4255126208ddfde3e82
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-03-03 07:43:07 -0800

    0x0C-09

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" rectangle"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class square """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        super().__init__(size, size, x, y, id)
+        self.size = size
+
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.height = value
+        self.width = value
+
+    def __str__(self):
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                         self.y, self.width)
