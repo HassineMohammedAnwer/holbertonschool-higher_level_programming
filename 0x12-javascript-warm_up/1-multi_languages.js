a1c3a0c7290c40f67d22e36fd9dee01a01071603
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-04-07 14:38:17 -0700

    0x12-01

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+
+const myVar1 = 'JavaScript is amazing';
+const myVar2 = 'C is fun';
+const myVar3 = 'Python is cool';
+console.log(myVar2);
+console.log(myVar3);
+console.log(myVar1);
