2df1a73236d6fa96a341f42471a68efc81d16c47
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-05-05 06:48:16 -0700

    0F-00

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 4:
+        print("Usage: {:s} <username> <password> <database>".format(argv[0]))
+        exit(99)
+
+    usrname = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
+
+    database = MySQLdb.Connect(
+        user=usrname,
+        passwd=passwd,
+        db=dbname,
+        port=3306)
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states")
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
