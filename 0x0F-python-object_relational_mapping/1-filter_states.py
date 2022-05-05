a70946f679977c6ac8b71df56d77da6bb252cddd
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-05-05 06:57:10 -0700

    0f-01

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+""" Usage: {:s} <username> <password> <database> """
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 4:
+        print("Usage: {:s} <username> <password> <database>".format(argv[0]))
+        exit(99)
+
+    usrname = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
+
+    database = MySQLdb.Connect(
+        user=usrname,
+        passwd=passwd,
+        db=dbname,
+        port=3306)
+    cursor = database.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
