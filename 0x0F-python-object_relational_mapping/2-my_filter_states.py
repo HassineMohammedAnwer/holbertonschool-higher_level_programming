7a223acfed00f8b07cbb82b48a845a9e6a404dc8
Hassine Mohamed Anouar <mohamedanouar.hassine@gmail.com>
2022-05-06 07:55:52 -0700

    0f-02

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+""" Usage: {:s} <username> <password> <database> """
+
+
+if __name__ == "__main__":
+    import sys
+    import MySQLdb
+    from sys import argv, exit
+
+    if len(argv) != 5:
+        print("Usage: {:s} <username> <password> <database>".format(argv[0]))
+        exit(99)
+
+    usrname = argv[1]
+    passwd = argv[2]
+    dbname = argv[3]
+
+    database = MySQLdb.Connect(
+        user=usrname,
+        passwd=passwd,
+        db=dbname,
+        port=3306)
+    cursor = database.cursor()
+    query = """SELECT * FROM states WHERE name = '{:s}'
+            ORDER BY states.id ASC""".format(sys.argv[4])
+    cursor.execute(query)
+    states = cursor.fetchall()
+    for row in states:
+        print(row)
