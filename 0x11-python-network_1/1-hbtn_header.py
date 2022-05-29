6424691ef51b2b9d0d7f634c163ad0ac301ea01b
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-05-29 21:00:14 +0100

    11-01

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-"""urllib.request.urlopen(sys.argv[1]) as response"""
-
-
+"""
+urllib.request.urlopen(sys.argv[1]) as response"""
 if __name__ == '__main__':
     import urllib.request
     import sys
+
     with urllib.request.urlopen(sys.argv[1]) as response:
         res = response.headers.get('X-Request-Id')
         print(res)
