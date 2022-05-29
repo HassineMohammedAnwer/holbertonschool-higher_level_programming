d38395ef8ac1d6fe5359996b113858bb8effa99e
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-05-29 20:22:12 +0100

    11-00

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""fetches https://intranet.hbtn.io/status"""
+import urllib.request
+
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+   print("Body response:")
+   html = response.read()
+   print("\t- type:", type(html))
+   print("\t- content:", html)
+   print("\t- utf8 content:", html.decode('utf-8'))
