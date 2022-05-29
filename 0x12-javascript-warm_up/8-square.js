14ab0bd9ea3e8f6dcda3df1977d02abde4270ea2
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-05-29 17:49:23 +0100

    12-07

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+
+const myVar = 'x';
+if (isNaN(process.argv[2])) {
+  console.log('Missing size');
+} else {
+  for (let i = 1; i <= process.argv[2]; i++) {
+    console.log(myVar.repeat(process.argv[2]));
+  }
+}
