972c80292e4f2df53c744d596bbb06281b660ac8
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-05-29 17:45:30 +0100

    12-07

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+
+const myVar = 'C is fun';
+if (isNaN(process.argv[2])) {
+  console.log('Missing number of occurrences');
+} else {
+  for (let i = 1; i <= process.argv[2]; i++) {
+    console.log(myVar);
+  }
+}
