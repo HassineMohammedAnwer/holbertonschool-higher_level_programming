efe8bb4d1036bb1422b40c1d006e72a3a86dac92
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 00:22:09 +0100

    6peakpep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""
-find the peek
-"""
+"""Find a peak"""
 
 
 def find_peak(l):
-    """
-        first or last element is peak element
-    """
+    """a function that finds a peak in a list of unsorted integers."""
     if l == []:
         return
     if l[0] >= l[1]:
         return l[0]
     if l[len(l) - 1] >= l[len(l) - 2]:
         return l[len(l) - 1]
-    for i in range(1, len(l) - 1):
-        if l[i] >= l[i - 1] and l[i] >= l[i + 1]:
+    for i in range(1, (len(l) - 1)):
+        if ((l[i] >= l[i - 1]) and (l[i] >= l[i + 1])):
             return l[i]
