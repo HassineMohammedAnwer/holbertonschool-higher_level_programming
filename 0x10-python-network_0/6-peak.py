33d5641037971fcd3eec4f9e9a76257a12e165d3
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 00:14:40 +0100

    6peakpep8

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -14,8 +14,6 @@ def find_peak(l):
         return l[0]
     if l[len(l) - 1] >= l[len(l) - 2]:
         return l[len(l) - 1]
-    """check for every other element"""
     for i in range(1, len(l) - 1):
-        """check if the neighbors are smaller"""
         if l[i] >= l[i - 1] and l[i] >= l[i + 1]:
             return l[i]
