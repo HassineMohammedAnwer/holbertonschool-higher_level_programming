831b4b1f728f4018cd756cdf9355fb770bf011e3
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 05:17:08 +0100

    12-11

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+if (process.argv.length <= 3) {
+    console.log(0);
+  } else {
+    const args = process.argv.map(Number);
+    len = process.argv.length
+    args.slice(2, len);
+    args.sort(function(a, b){return a - b});
+    console.log(args[len - 2]);
+  }
