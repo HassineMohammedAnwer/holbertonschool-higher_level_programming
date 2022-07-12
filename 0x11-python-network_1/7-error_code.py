a45dfba3be6bf85f91d00a5795c02afb9be4c9f5
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 02:50:30 +0100

    11-07

---
+++ 0x11-python-network_1/7-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""5e52e160-c822-4669-8b3a-8b3bbca7b090
+    -
+    -"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    response = requests.get(sys.argv[1])
+    if (response.status_code >= 400):
+        print(Error code: {}'.format(response.status_code))
+    else:
+        print(response.text)
