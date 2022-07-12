296f00ab138525bd9eedd1a27b9d8594f5eb0cc3
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 04:37:01 +0100

    12-10fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -4,11 +4,12 @@ function factorial (x) {
   let res = 1;
   if (isNaN(x) || (x == 0)) {
     return (1);
-  } else {
+  }
+  else {
     for (let i = 1; i <= parseInt(x); i++) {
       res = res * i;
     }
-}
+  }
   return (res);
 }
 console.log(factorial(x));
