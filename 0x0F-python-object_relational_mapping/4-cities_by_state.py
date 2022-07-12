740a98f92d98f12b55e6595cbb001d494646a82a
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 06:22:42 +0100

    0f-4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,7 +6,7 @@ if __name__ == "__main__":
     import MySQLdb
     from sys import argv, exit
 
-    if len(argv) != 5:
+    if len(argv) != 4:
         print("Usage: {:s} <username> <password> <database>".format(argv[0]))
         exit(99)
 
