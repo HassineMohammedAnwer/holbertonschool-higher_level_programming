51fcd2aab7902c932bf4cd81f746f3456e2837cd
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 03:58:43 +0100

    11-10_github_api

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""5e52e160-c822-4669-8b3a-8b3bbca7b090
+    -
+    -"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    response = requests.get("https://api.github.com/user",
+                     auth=(sys.argv[1], sys.argv[2]))
+    jresponse = response.json()
+    try:
+        print(jresponse['id'])
+    except KeyError:
+        print(None)
