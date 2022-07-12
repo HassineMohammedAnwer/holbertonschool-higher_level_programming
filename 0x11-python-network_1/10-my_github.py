130054c28d3a46497bf2d476716f240f71c6bc12
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 04:01:41 +0100

    11-10_github_api

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -8,9 +8,9 @@ import sys
 
 if __name__ == '__main__':
     response = requests.get("https://api.github.com/user",
-                     auth=(sys.argv[1], sys.argv[2]))
+                            auth=(sys.argv[1], sys.argv[2]))
     jresponse = response.json()
     try:
         print(jresponse['id'])
     except KeyError:
-        print(None)
+        print('None')
