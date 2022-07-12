65f0ba75bdd285bb69525bbf84a403066d72f67f
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 02:44:02 +0100

    11-06

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -9,5 +9,5 @@ import sys
 if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
-    response = requests.get(url, data={'email': email}).text
+    response = requests.get(url, email).text
     print(response.text)
