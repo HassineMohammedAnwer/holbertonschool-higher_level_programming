24910893aac1a3b2f7beb8c94fe46dc96a97e2e7
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 04:35:47 +0100

    12-10fix

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
-
-let res = 1;
-if (isNaN(process.argv[2]) || (process.argv[2] == 0)) {
-  console.log(1);
-  return;
-} else {
-  for (let i = 1; i <= parseInt(process.argv[2]); i++) {
-    res = res * i;
-  }
+let x = process.argv[2];
+function factorial (x) {
+  let res = 1;
+  if (isNaN(x) || (x == 0)) {
+    return (1);
+  } else {
+    for (let i = 1; i <= parseInt(x); i++) {
+      res = res * i;
+    }
 }
-console.log(res);
+  return (res);
+}
+console.log(factorial(x));
