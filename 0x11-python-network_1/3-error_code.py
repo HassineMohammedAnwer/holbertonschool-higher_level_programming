e3a1168da1df225482881d01d22e089538b0b6bf
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 01:53:47 +0100

    fx 11-3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,13 +1,12 @@
 #!/usr/bin/python3
 """urllib.request.urlopen(sys.argv[1]) as response"""
+
+
 from urllib import request
 from urllib.error import HTTPError
 from sys import argv
 
-
 if __name__ == '__main__':
-
-    """mjk mk jm qaae dok"""
     url = argv[1]
     try:
         with request.urlopen(url) as response:
