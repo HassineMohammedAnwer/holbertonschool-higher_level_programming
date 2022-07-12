7b9c52edd71d5f99842f357b062be5b7752b2c74
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 01:56:20 +0100

    fx 11-3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python3
 """urllib.request.urlopen(sys.argv[1]) as response"""
-
-
 from urllib import request
 from urllib.error import HTTPError
 from sys import argv
+
 
 if __name__ == '__main__':
     url = argv[1]
     try:
         with request.urlopen(url) as response:
             print(response.read().decode('utf-8'))
-    except error.HTTPError as e:
+    except HTTPError as e:
             print("Error code: {}".format(e.code))
