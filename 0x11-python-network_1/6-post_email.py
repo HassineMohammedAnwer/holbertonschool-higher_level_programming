a74db34de36e0e7ca2cfa907602f84219130f40f
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 02:40:04 +0100

    11-06

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""5e52e160-c822-4669-8b3a-8b3bbca7b090
+    -
+    -"""
+import requests
+import sys
+
+
+if __name__ == '__main__':
+    url = sys.argv[1]
+    email = sys.argv[2]
+    response = requests.get(url, data=email).text
+    print(response.text)
