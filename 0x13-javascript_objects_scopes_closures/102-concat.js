2882931e6923636df6c47d91dfa20b410b7af359
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 04:33:00 +0100

    13-102fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,7 @@ const f1 = process.argv[2];
 const f2 = process.argv[3];
 const f3 = process.argv[4];
 const data1 = fs.readFileSync(f1,
-            {encoding:'utf8', flag:'r'});
+  { encoding: 'utf8', flag: 'r' });
 const data2 = fs.readFileSync(f2,
-            {encoding:'utf8', flag:'r'});
+  { encoding: 'utf8', flag: 'r' });
 fs.writeFileSync(f3, data1 + data2);
