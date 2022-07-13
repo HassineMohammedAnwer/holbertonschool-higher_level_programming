941e81e97ee2e0401838daeafdb295dd518afe90
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 04:30:14 +0100

    13-102fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-//const fs = require('fs');
-const f1 =process.argv[2];
-const f2 =process.argv[3];
-const f3 =process.argv[4];
-
-
-//fs.writeFileSync(f3, contents + contents2);
-
-var fs = require('fs');
-fs.readFile(f1, 'utf8', (err, data) => {
-});
-var fs = require('fs');
-fs.readFile(f2, 'utf8', (err, data2) => {
-});
-fs.writeFileSync(f3, data + data2);
+// Include fs module
+const fs = require('fs');
+const f1 = process.argv[2];
+const f2 = process.argv[3];
+const f3 = process.argv[4];
+const data1 = fs.readFileSync(f1,
+            {encoding:'utf8', flag:'r'});
+const data2 = fs.readFileSync(f2,
+            {encoding:'utf8', flag:'r'});
+fs.writeFileSync(f3, data1 + data2);
