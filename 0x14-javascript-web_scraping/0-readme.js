d4b90d0335a59c3002ac0eb06b0029a37a7692f4
HAssineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-13 04:45:54 +0100

    14-00

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const fs = require('fs');
+
+try {
+  const f1 = process.argv[2];
+  const data = fs.readFileSync(f1,
+  { encoding: 'utf8', flag: 'r' });
+  console.log(data);
+} catch (err) {
+  console.log(err);
+}
