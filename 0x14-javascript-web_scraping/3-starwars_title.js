8353667d4c0eda0fadadffeec291aacf4e5e0da1
HassineMohamedAnwer <mohamedanouar.hassine@gmail.com>
2022-07-12 21:45:58 -0700

    14-03fx

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,9 +4,5 @@ const request = require('request');
 const ap = 'https://swapi-api.hbtn.io/api/films/';
 
 request.get(ap + process.argv[2], function (error, response, body) {
-  if (error) {
-    console.log(error);
-  } else {
-    console.log(JSON.parse(body).title);
-  }
+  console.log(JSON.parse(body).title);
 });
