de4af459e555201522b740dd3940ca19489dbee7
HassineMohamedAnwer <91066644+HassineMohamedAnwer@users.noreply.github.com>
2022-09-21 18:56:48 +0100

    Update 101-script.js

--- 0x15-javascript-web_jquery/101-script.js
+++ 0x15-javascript-web_jquery/101-script.js
@@ -1,14 +1,14 @@
-(function () {
+window.onload = function () {
   $('DIV#add_item').click(function () {
-    $('UL.my_list').appendTo('<li>Item</li>');
+    $('UL.my_list').append('<li>Item</li>');
   });
 
   $('DIV#remove_item').click(function () {
-    $('UL.my_list li').remove();
+    $('UL.my_list li').last().remove();
     counter();
   });
 
   $('DIV#clear_list').click(function () {
-    $('UL.my_list li').empty();
+    $('UL.my_list li').remove();
   });
-})();
+}
